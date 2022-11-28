d9b5876533eaf7a80cc4f0b56adad5c56fc5c6d3
Md Ashik Billah <ashikbillah699@gmail.com>
2022-11-29 04:57:34 +0600

    about part setup

--- src/Pages/Home/OurPolicy/OurPolicy.js
+++ src/Pages/Home/OurPolicy/OurPolicy.js
@@ -2,8 +2,16 @@ import React from 'react';
 
 const OurPolicy = () => {
     return (
-        <div>
-            this is our policy
+        <div className="hero  my-20">
+            <div className="hero-content flex-col lg:flex-row">
+                <img alt='' src='https://s.yimg.com/uu/api/res/1.2/HIzpqfITrRI8jM6YiFUWFA--~B/aD02MDE7dz02MDA7YXBwaWQ9eXRhY2h5b24-/https://www.blogcdn.com/www.engadget.com/media/2008/12/xps13_main.jpg' />
+                <div>
+                    <h1 className="text-5xl font-bold">OUR POLICY</h1>
+                    <p className="py-6">YOUR HAPPINESS IS OUR SUCCESS</p>
+                    <p className="py-6 leading-7	">Laptop shall be the property of the Company at all times and the employee will not have any right or interest in the said asset except using such asset during the employment or for such duration as may be decided by the Company. Employee must ensure that the laptop is being used only for official purposes and in the course of the rightful discharge of their duties and not for generating, transmitting, corresponding any content that is contrary to Company policies.  This may lead to the employee being subject to disciplinary or any other appropriate action as per Company policies. B. Laptop Security Controls ,All laptops acquired for employees on behalf of the Company shall be deemed to be Company property. Each employee issued with a laptop shall be responsible for the security of that laptop, regardless of whether the laptop is used in the office, at the employee’s place of residence, or in any other location such as a hotel, conference room, car or airport. Employee shall ensure security of the laptop in each of the following domains as per the stated guidelines. Laptops must compulsorily be protected by a username and password.  </p>
+                    <button className="btn btn-black">Learn more</button>
+                </div>
+            </div>
         </div>
     );
 };
